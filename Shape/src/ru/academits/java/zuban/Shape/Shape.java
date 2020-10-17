ae26bc89ec1697451c9393ce33f0e2ce80c4281e
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2020-10-17 14:08:20 +0700

    Добавим класс квадрат

---
+++ Shape/src/ru/academits/java/zuban/Shape/Shape.java
@@ -0,0 +1,8 @@
+package ru.academits.java.zuban.Shape;
+
+ interface Shape {
+  double getWidth();
+  double getHeight();
+  double getArea();
+  double getPerimeter();
+}
---
+++ Shape/src/ru/academits/java/zuban/Shape/ShapeProgram.java
@@ -0,0 +1,6 @@
+package ru.academits.java.zuban.Shape;
+
+public class ShapeProgram {
+    public static void main(String[] args) {
+    }
+}
