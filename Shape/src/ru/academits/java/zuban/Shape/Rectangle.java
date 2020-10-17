a23336de40ca9682ada43729232c4a430ac92746
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2020-10-17 14:14:02 +0700

    Добавим класс квадрат и прямоугольник

---
+++ Shape/src/ru/academits/java/zuban/Shape/Rectangle.java
@@ -0,0 +1,27 @@
+package ru.academits.java.zuban.Shape;
+
+class Rectangle implements Shape{
+    private final double width;
+    private final double height;
+
+    Rectangle(double width,double height) {
+        this.width = width;
+        this.height=height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return width*height;
+    }
+
+    public double getPerimeter() {
+        return 2*width+2*height;
+    }
+}
---
+++ Shape/src/ru/academits/java/zuban/Shape/Square.java
@@ -0,0 +1,25 @@
+package ru.academits.java.zuban.Shape;
+
+class Square implements Shape {
+    private final double width;
+
+    Square(double width) {
+        this.width = width;
+    }
+
+    public double getArea() {
+        return width * width;
+    }
+
+    public double getPerimeter() {
+        return 4 * width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight(){
+        return 0;
+    }
+}
