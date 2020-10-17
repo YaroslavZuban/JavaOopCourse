a316db51fbd9f307ae260b1dba5736e0c7498854
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2020-10-17 14:29:53 +0700

    не дописал класс верно

---
+++ Shape/src/ru/academits/java/zuban/Shape/Triangle.java
@@ -0,0 +1,39 @@
+package ru.academits.java.zuban.Shape;
+
+class Triangle implements Shape {
+    private final double x1;
+    private final double y1;
+    private final double x2;
+    private final double y2;
+    private final double x3;
+    private final double y3;
+
+    Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+    }
+
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+}
