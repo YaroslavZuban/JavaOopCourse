f940c18690c8458b0b71f1b9474cf52c0c4f6066
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2021-02-16 20:22:41 +0700

    добавил весь код!

--- Vector/src/ru/academits/java/zuban/vector/Vector.java
+++ Vector/src/ru/academits/java/zuban/vector/Vector.java
@@ -1,5 +1,241 @@
 package ru.academits.java.zuban.vector;
 
-public class Vector {
+import java.util.Arrays;
 
+public class Vector {
+    private int size;
+    private double[] vector;
+
+    Vector(int n) {//размерность n, все компоненты равны 0
+        if (n <= 0) {
+            doRisky();
+        }
+
+        vector = new double[n];
+        this.size = n;
+
+        for (int i = 0; i < size; i++) {
+            this.vector[i] = 0;
+        }
+    }
+
+    Vector(Vector vectorCopy) {//конструктор копирования
+        this.size = vectorCopy.getSize();
+        vector = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            this.vector[i] = vectorCopy.getComponents(i);
+        }
+    }
+
+    Vector(double[] array) {//заполнение вектора значениями из массив
+        this.size = array.length;
+        vector = new double[size];
+
+        System.arraycopy(array, 0, this.vector, 0, size);
+    }
+
+    Vector(int n, double[] array) {//заполнение вектора значениями из массива. Если длина массива меньше n, то считать что в остальных компонентах 0
+        if (n <= 0) {
+            doRisky();
+        }
+
+        this.size = array.length;
+
+        if (n > array.length) {
+            vector = new double[n];
+            for (int i = 0; i < n; i++) {
+                if (array.length > i) {
+                    this.vector[i] = array[i];
+
+                } else {
+                    this.vector[i] = 0;
+                }
+            }
+        } else {
+            vector = new double[array.length];
+
+            System.arraycopy(array, 0, this.vector, 0, array.length);
+        }
+    }
+
+
+    public double[] getVector() {//выдает массив, для копирования
+        return vector;
+    }
+
+    public int getSize() {//получения размерности вектора
+        return vector.length;
+    }
+
+    private void doRisky() throws IllegalArgumentException {//ввывод ошибки, решил перенести в отдельную функцию чтобы не дублировать код
+        throw new IllegalArgumentException("Размерность меньше нуля! ");
+    }
+
+    @Override
+    public String toString() {//переопределил метод вывода вектора
+        return Arrays.toString(vector);
+    }
+
+    public boolean equals(Vector vec) {
+        return (getSize() == vec.getSize() && hashCode() == vec.hashCode());
+    }
+
+    @Override
+    public int hashCode() {
+        if (vector == null) {
+            return 0;
+        }
+
+        final int prime = 37;
+        int result = 1;
+        result = prime * result + size;
+
+        for (double arr : vector) {
+            long longBits = Double.doubleToLongBits(arr);
+            result = prime * result + (int) (longBits - (longBits >>> 32));
+        }
+
+        return result;
+    }
+
+    public Vector addition(Vector vec) {//сложение
+        int indexMax = Math.max(getSize(), vec.getSize());
+        Vector number;
+
+        if (vec.getSize() >= getSize()) {
+            number = new Vector(indexMax, getVector());
+
+            for (int i = 0; i < indexMax; i++) {
+                double sum = number.getComponents(i) + vec.getComponents(i);
+                number.setComponents(i, sum);
+            }
+        } else {
+            number = new Vector(indexMax, vec.getVector());
+
+            for (int i = 0; i < indexMax; i++) {
+                double sum = number.getComponents(i) + getComponents(i);
+                number.setComponents(i, sum);
+            }
+        }
+
+        return number;
+    }
+
+    public Vector subtraction(Vector vec) {//вычитание
+        int indexMax = Math.max(getSize(), vec.getSize());
+        Vector number;
+
+        if (vec.getSize() >= getSize()) {
+            number = new Vector(indexMax, getVector());
+
+            for (int i = 0; i < indexMax; i++) {
+                double sum = number.getComponents(i) - vec.getComponents(i);
+                number.setComponents(i, sum);
+            }
+        } else {
+            number = new Vector(indexMax, vec.getVector());
+
+            for (int i = 0; i < indexMax; i++) {
+                double sum = number.getComponents(i) - getComponents(i);
+                number.setComponents(i, sum);
+            }
+        }
+
+        return number;
+    }
+
+
+    public void multiplicationScalar(int multiplier) {//умножение на скаляр
+        for (int i = 0; i < vector.length; i++) {
+            this.vector[i] *= multiplier;
+        }
+    }
+
+    public void reversal() {// умножение вектора на -1
+        for (int i = 0; i < vector.length; i++) {
+            this.vector[i] *= -1;
+        }
+
+        Arrays.sort(this.vector);
+    }
+
+    public double getLength() {//получение длинный вектора
+        double sumVariables = 0;
+
+        for (double mas : vector) {
+            sumVariables += Math.pow(mas, 2);
+        }
+
+        return Math.sqrt(sumVariables);
+    }
+
+    public double getComponents(int index) {//получение ячейки массива по индексу
+        return vector[index];
+    }
+
+    public void setComponents(int index, double number) {//установка нового числа по индексу
+        this.vector[index] = number;
+    }
+
+    public static Vector addition(Vector vec1, Vector vec2) {//Сложение двух векторов – должен создаваться новый вектор
+        vec1.addition(vec2);
+        return new Vector(vec1);
+    }
+
+    public static Vector subtraction(Vector vec1, Vector vec2) {//Вычитание векторов – должен создаваться новый вектор
+        vec1.subtraction(vec2);
+        return new Vector(vec1);
+    }
+
+    public static Vector productScalar(Vector vec1, Vector vec2) {//Скалярное произведение векторов
+        int indexMax = Math.max(vec1.getSize(), vec2.getSize());
+        int indexMin = Math.min(vec1.getSize(), vec2.getSize());
+
+        double[] array = new double[indexMax];
+
+        if (vec1.getSize() == indexMax) {
+            for (int i = 0; i < indexMax; i++) {
+                if (i < indexMin) {
+                    array[i] = vec1.vector[i];
+                } else {
+                    array[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < indexMax; i++) {
+                array[i] *= vec2.getComponents(i);
+            }
+        } else {
+            for (int i = 0; i < indexMax; i++) {
+                if (i < indexMin) {
+                    array[i] = vec2.getComponents(i);
+                } else {
+                    array[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < indexMax; i++) {
+                array[i] *= vec1.vector[i];
+            }
+        }
+
+
+        return new Vector(array);
+    }
+
+    private double[] arrayPlusNull(int size) {
+        double[] arrayNew = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            if (i < vector.length) {
+                arrayNew[i] = vector[i];
+            } else {
+                arrayNew[i] = 0;
+            }
+        }
+
+        return arrayNew;
+    }
 }
+
--- Vector/src/ru/academits/java/zuban/vector/VectorProgram.java
+++ Vector/src/ru/academits/java/zuban/vector/VectorProgram.java
@@ -1,5 +1,68 @@
 package ru.academits.java.zuban.vector;
 
 public class VectorProgram {
+    public static void main(String[] args) {
+        double[] arrayTest = {2, 3, 5, 6, 9, 7, 5};
+        Vector test = new Vector(arrayTest);
 
+        System.out.println("Начианем тестировать код: \n Программа _1_: ");
+        Vector vector = new Vector(8);
+        String line = vector.toString();
+        System.out.println(line);
+        System.out.println("Размерноть равна 8 следовательно данный вектор: " + vector.getSize());
+
+        System.out.println("Программа _2_: ");
+        try {
+            Vector vector1 = new Vector(-2);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Ошибка произошла! Поздравляю! Привет");
+        } finally {
+            System.out.println();
+        }
+
+        System.out.println("Программа _3_: ");
+
+        Vector vector2 = new Vector(vector);
+        String line2 = vector2.toString();
+        System.out.println(line2);
+        System.out.println("Размерноть равна 8 следовательно данный вектор: " + vector2.getSize());
+
+        System.out.println("Программа _4_: ");
+
+        double[] arrayTest3 = {1, 1, 1, 1};
+        Vector vector3 = new Vector(6, arrayTest3);
+
+        String line3 = vector3.toString();
+        System.out.println(line3);
+
+        Vector vectorArray = new Vector(vector3.addition(test));
+        System.out.println("Прибавление: " + vectorArray.toString());
+
+        Vector vectorArray1 = new Vector(vector3.subtraction(test));
+        System.out.println("Вычитание: " + vectorArray1.toString());
+
+        System.out.println("Программа _5_: ");
+
+        double[] arrayTest4 = {1, 1, 1, 1};
+        Vector vector4 = new Vector(4, arrayTest4);
+
+        vector4.multiplicationScalar(2);
+        System.out.println("Скалярная умножение: " + vector4.toString());
+
+        vector4.reversal();
+        System.out.println("Умноженние на -1(разворот): " + vector4.toString());
+
+        System.out.println("Программа _6_: ");
+
+        double[] arrayTest5 = {1, 2, 1, 1};
+        Vector vector5 = new Vector(3, arrayTest5);
+
+        System.out.println("Получение длинны вектора: " + vector5.getLength());
+
+        System.out.println("Получение по индексу: " + vector5.getComponents(1));
+
+        vector5.setComponents(1, 0);
+        System.out.println("Установка и получение по индексу: " + vector5.getComponents(1));
+    }
 }
+
